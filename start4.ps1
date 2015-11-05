c5a3fa4af739ffc0c6887126cc0e86d50de4a66f
David Guimbellot <David Guimbellot>
2015-11-05 16:55:21 -0500

    unsigned file

---
+++ start4.ps1
@@ -0,0 +1,4 @@
+Start-Service -displayname "Performance Counter DLL Host"
+Start-Service -displayname "Performance Logs & Alerts"
+Start-Service -displayname "Remote Procedure Call (RPC) Locator"
+Start-Service -displayname "Remote Procedure Call (RPC)"
